a0caaad1e3f104d80fa4825f0981b4820681e2f8
zzangoobrother <com1368@naver.com>
2021-12-28 15:59:30 +0900

    [feat]guestbook list 화면 및 controller 구현

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -2,17 +2,32 @@ package org.zerock.guestbook.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
 public class GuestbookController {
 
-  @GetMapping({"/", "/list"})
+  private final GuestbookService guestbookService;
+
+  public GuestbookController(GuestbookService guestbookService) {
+    this.guestbookService = guestbookService;
+  }
+
+  @GetMapping("/")
   public String list() {
     log.info("list................");
     return "/guestbook/list";
+  }
+
+  @GetMapping("/list")
+  public void list(PageRequestDTO pageRequestDTO, Model model) {
+    log.info("list................" + pageRequestDTO);
+    model.addAttribute("result", guestbookService.getList(pageRequestDTO));
   }
 }
