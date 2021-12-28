6e0e3a99e9e7a91eb3ffc5ee850b4a2bb858ee10
zzangoobrother <com1368@naver.com>
2021-12-28 09:48:32 +0900

    [feat]GuestbookService dtoToEntity 메소드 생성 및 register 구현

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,8 +1,18 @@
 package org.zerock.guestbook.service;
 
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
 
   Long register(GuestbookDTO dto);
+
+  default Guestbook dtoToEntity(GuestbookDTO dto) {
+    return Guestbook.builder()
+        .gno(dto.getGno())
+        .title(dto.getTitle())
+        .content(dto.getContent())
+        .writer(dto.getWriter())
+        .build();
+  }
 }
