2a690087ec1b4349478a5c6192bdc4ddccff6e7e
zzangoobrother <com1368@naver.com>
2021-12-28 10:18:54 +0900

    [feat]GuestbookServiceImpl 구현 완료

--- src/main/java/org/zerock/guestbook/dto/GuestbookDTO.java
+++ src/main/java/org/zerock/guestbook/dto/GuestbookDTO.java
@@ -6,12 +6,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class GuestbookDTO {
 
   private Long gno;
--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -4,10 +4,17 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
 public class GuestbookServiceImpl implements GuestbookService {
+
+  private final GuestbookRepository guestbookRepository;
+
+  public GuestbookServiceImpl(GuestbookRepository guestbookRepository) {
+    this.guestbookRepository = guestbookRepository;
+  }
 
   @Override
   public Long register(GuestbookDTO dto) {
@@ -16,6 +23,9 @@ public class GuestbookServiceImpl implements GuestbookService {
 
     Guestbook entity = dtoToEntity(dto);
     log.info(entity);
-    return null;
+
+    guestbookRepository.save(entity);
+
+    return entity.getGno();
   }
 }
