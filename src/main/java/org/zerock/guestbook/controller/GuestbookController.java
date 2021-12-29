3cade2e7c560f28327b95f3d41157b37daf3cbcb
zzangoobrother <com1368@naver.com>
2021-12-29 21:13:38 +0900

    [feat] 게시글 등록 구현

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -4,7 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -29,5 +32,21 @@ public class GuestbookController {
   public void list(PageRequestDTO pageRequestDTO, Model model) {
     log.info("list................" + pageRequestDTO);
     model.addAttribute("result", guestbookService.getList(pageRequestDTO));
+  }
+
+  @GetMapping("/register")
+  public void register() {
+    log.info("register get...");
+  }
+
+  @PostMapping("/register")
+  public String registerPost(GuestbookDTO guestbookDTO, RedirectAttributes redirectAttributes) {
+    log.info("dto..." + guestbookDTO);
+
+    Long gno = guestbookService.register(guestbookDTO);
+
+    redirectAttributes.addFlashAttribute("msg", gno);
+
+    return "redirect:/guestbook/list";
   }
 }
