5e71ff7ad2b972506365f55466dcca1522b72d00
zzangoobrother <com1368@naver.com>
2021-12-30 10:31:37 +0900

    [feat] service 계층 수정, 삭제 구현

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -51,4 +51,23 @@ public class GuestbookServiceImpl implements GuestbookService {
 
     return result.map(this::entityToDto).orElse(null);
   }
+
+  @Override
+  public void remove(Long gno) {
+    guestbookRepository.deleteById(gno);
+  }
+
+  @Override
+  public void modify(GuestbookDTO dto) {
+    Optional<Guestbook> result = guestbookRepository.findById(dto.getGno());
+
+    if (result.isPresent()) {
+      Guestbook entity = result.get();
+
+      entity.changeTitle(dto.getTitle());
+      entity.changeContent(dto.getContent());
+
+      guestbookRepository.save(entity);
+    }
+  }
 }
